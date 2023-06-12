5023d516abc236cbcc96321a60aa9da8a1d65cfb
ZoeyYanling <122327135+ZoeyYanling@users.noreply.github.com>
2023-06-12 11:11:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+//Add a listener for click events on submit button
+var submitBtn = $('.submitBtn');
+submitBtn.on('click',function(){
+    var name = $('#name').text();
+    var nameInput = $('#nameInput').val();
+    var email = $('#email').text();
+    var emailInput = $('#emailInput').val();
+    var interest = $('#interest').text();
+    var interestInput = $('#interestInput').val();
+    localStorage.setItem(name,nameInput);
+    localStorage.setItem(email,emailInput);
+    localStorage.setItem(interest,interestInput);
+
+    console.log(nameInput);
+    console.log(emailInput);
+    console.log(interestInput);
+});
+
+
+
