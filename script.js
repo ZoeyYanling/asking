61fd5e32a905c88a1fdd1b7274a3a33df5df9c37
ZoeyYanling <122327135+ZoeyYanling@users.noreply.github.com>
2023-06-13 07:04:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 var submitBtn = $('.submitBtn');
 submitBtn.on('click', function(){
     var name = $('#name').text();
-    var nameInput = $('#nameInput').val();
+    var nameInput = $('#inputName').val();
     var email = $('#email').text();
-    var emailInput = $('#emailInput').val();
+    var emailInput = $('#inputEmail').val();
     var interest = $('#interest').text();
-    var interestInput = $('#interestInput').val();
+    var interestInput = $('#inputInterest').val();
     localStorage.setItem(name,nameInput);
     localStorage.setItem(email,emailInput);
     localStorage.setItem(interest,interestInput);
