9366a1b8b84048991b76ddebd9150b294027c43b
ZoeyYanling <122327135+ZoeyYanling@users.noreply.github.com>
2023-06-13 06:46:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
 //Add a listener for click events on submit button
 var submitBtn = $('.submitBtn');
-submitBtn.on('click',function(){
-    var name = $（this）.siblings('#name').text();
-    var nameInput = $（this）.siblings('#nameInput').val();
-    var email = $（this）.siblings('#email').text();
-    var emailInput = $（this）.siblings('#emailInput').val();
-    var interest = $（this）.siblings('#interest').text();
-    var interestInput = $（this）.siblings('#interestInput').val();
+submitBtn.on('click', function(){
+    var name = $('#name').text();
+    var nameInput = $('#nameInput').val();
+    var email = $('#email').text();
+    var emailInput = $('#emailInput').val();
+    var interest = $('#interest').text();
+    var interestInput = $('#interestInput').val();
     localStorage.setItem(name,nameInput);
     localStorage.setItem(email,emailInput);
     localStorage.setItem(interest,interestInput);
-
+    
 });
 
 
